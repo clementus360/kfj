06ffab17e0c42ab5f8d80544916f6b5043e9717f
clementus360 <clementus360@gmail.com>
2024-02-11 22:13:50 +0200

    revamp

--- src/app/cars/car.tsx
+++ src/app/cars/car.tsx
@@ -4,9 +4,9 @@ import { car } from "@/utils/types";
 export default function Car({carId, location, price, cover, date_added}: car) {
 
   return (
-    <div className="flex flex-col gap-4 w-full lg:w-min lg:min-w-[360px] bg-slate-200 p-4 rounded-lg shadow-lg">
+    <div className="flex flex-col gap-4 w-full lg:w-min lg:min-w-[360px] bg-slate-200 p-4 rounded-md shadow-lg">
 
-        <img src={cover} alt="car name" className="bg-black w-full rounded-lg aspect-video" />
+        <img src={cover} alt="car name" className="bg-black w-full rounded-md aspect-video" />
 
         <div>
             <div className="flex gap-2 items-center">
@@ -21,7 +21,7 @@ export default function Car({carId, location, price, cover, date_added}: car) {
 
         </div>
 
-        <a href={`/properties/${carId}`}><button className="bg-black hover:bg-slate-900 text-white rounded-full w-full py-2 font-bold">More Info</button></a>
+        <a href={`/properties/${carId}`}><button className="bg-black hover:bg-slate-900 text-white rounded-lg w-full py-2 font-bold">More Info</button></a>
 
         <div>
           <p className="text-xs text-slate-400">{date_added?.toLocaleString()}</p>
