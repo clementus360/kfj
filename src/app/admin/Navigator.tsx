f1881df7ec5e627b8d204649496ddfe9162879e5
clementus360 <clementus360@gmail.com>
2024-02-20 02:18:25 +0200

    refactor navigation from layout

---
+++ src/app/admin/Navigator.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+
+export default function Navigator() {
+
+    const path = usePathname()
+
+  return (
+    <div>
+        {path &&
+                <section className="flex w-full lg:w-8/12 m-auto shadow-lg justify-between items-center rounded-md overflow-hidden">
+                    <a href="/admin/houses" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/houses"? '':'bg-white'}`}>Houses</button></a>
+                    <div className="w-[2px] h-12 bg-slate-400"></div>
+                    <a href="/admin/cars" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/cars"? '':'bg-white'}`}>Cars</button></a>
+                    <div className="w-[2px] h-12 bg-slate-400"></div>
+                    <a href="/admin/ads" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/ads"? '':'bg-white'}`}>Ads</button></a>
+                </section>
+            }
+    </div>
+  )
+}
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,26 +1,14 @@
-"use client"
-
-import { usePathname } from "next/navigation";
+import Navigator from "./Navigator";
 
 export default function layout({children}: Readonly<{
     children: React.ReactNode;
   }>) {
 
-    const path = usePathname()
 
     return (
         <div className="flex flex-col gap-8 w-full  lg:w-8/12 m-auto pt-32 ">
 
-            {path &&
-                <section className="flex w-full lg:w-8/12 m-auto shadow-lg justify-between items-center rounded-md overflow-hidden">
-                    <a href="/admin/houses" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/houses"? '':'bg-white'}`}>Houses</button></a>
-                    <div className="w-[2px] h-12 bg-slate-400"></div>
-                    <a href="/admin/cars" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/cars"? '':'bg-white'}`}>Cars</button></a>
-                    <div className="w-[2px] h-12 bg-slate-400"></div>
-                    <a href="/admin/ads" className="w-full h-full"><button className={`transition-all w-full py-4 ${path==="/admin/ads"? '':'bg-white'}`}>Ads</button></a>
-                </section>
-            }
-
+            <Navigator />
 
             <div>
                 {children}
