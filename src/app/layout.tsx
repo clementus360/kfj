e4f8872c8c1de82e959a85fca50256148fc823cc
clementus360 <clementus360@gmail.com>
2024-02-21 17:03:26 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Willy Investment Group",
@@ -21,6 +22,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
